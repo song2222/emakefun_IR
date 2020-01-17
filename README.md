7e2ebc95fa2dcb4a24e10d10743e61c129dc49b3
song2222 <2669036912@qq.com>
2020-01-17 10:03:43 +0800

    gews

---
+++ README.md
@@ -0,0 +1,14 @@
+ # OSOYOO_IR for micro-bit
+
+Extension for OSOYOO_IR
+
+Note: IR receiver for OSOYOO receiver
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ enums.d.ts
@@ -0,0 +1,49 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+
+    declare enum RemoteButton {   
+	  num0 = 0x19,
+	  num1 = 0x45,
+	  num2 = 0x46,
+	  num3 = 0x47,
+	  num4 = 0x44,
+	  num5 = 0x40,
+	  num6 = 0x43,
+	  num7 = 0x7,
+	  num8 = 0x15,
+	  num9 = 0x9,
+	  redR = 0xd,
+	  redL= 0x16,
+	  up = 0x18,
+	  left = 0x8,
+	  ok = 0x1c,
+	  right = 0x5a,
+	  down = 0x52,
+    }
+declare namespace Mbit_IR {
+}
+
+// Auto-generated. Do not edit. Really.
